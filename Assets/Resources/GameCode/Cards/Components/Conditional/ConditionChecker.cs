87834d11e599f87dc5c92dcd7cd9876aa7d8b144
Edd Sherlock <pulsarflux@googlemail.com>
2019-04-07 11:10:39 +0100

    Add conditional components
    Modular components that check a condition on the entity and only allow eg the module to fire or the action to work if true.

---
+++ Assets/Resources/GameCode/Cards/Components/Conditional/ConditionChecker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.GameCode.Cards.Components.Conditional
+{ 
+    [Serializable()]
+    public class ConditionChecker
+    {
+        public ConditionChecker(List<Loading.ConditionalData> conditions)
+        {
+            if (conditions != null &&
+                conditions.Count > 0)
+            {
+                mConditions = new List<ConditionalComponent>();
+                foreach (Loading.ConditionalData data in conditions)
+                {
+                    mConditions.Add(ConditionalComponent.CreateFromData(data));
+                }
+            }
+            else
+            {
+                mConditions = null;
+            }
+        }
+
+        public bool Check(Entities.Entity entity)
+        {
+            bool result = true;
+            if (mConditions != null)
+            {
+                foreach (ConditionalComponent cc in mConditions)
+                {
+                    result = result && cc.Check(entity);
+                }
+            }
+            return result;
+        }
+
+        private List<ConditionalComponent> mConditions;
+    }
+}
+
---
+++ Assets/Resources/GameCode/Cards/Components/Conditional/ConditionalComponent.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Assets.GameCode.Cards.Components.Conditional
+{ 
+    [Serializable()]
+    public abstract class ConditionalComponent
+    {
+        public ConditionalComponent()
+        {
+        }
+
+        public abstract bool Check(Entities.Entity entity);
+
+        public static ConditionalComponent CreateFromData(Loading.ConditionalData data)
+        {
+            ConditionalComponent newConditional = null;
+            // TODO - Keep up to date
+            switch (data.mType)
+            {
+                case "HasStatus":
+                    newConditional = new HasStatusConditional(data);
+                    break;
+                case "NoStatus":
+                    newConditional = new NoStatusConditional(data);
+                    break;
+                case "OrderNotUsed":
+                    newConditional = new OrderNotUsedConditional(data);
+                    break;
+                default:
+                    UnityEngine.Debug.Log("Unhandled conditional type: " + data.mType);
+                    UnityEngine.Debug.DebugBreak();
+                    break;
+            }
+            return newConditional;
+        }
+    }
+
+    [Serializable()]
+    public class HasStatusConditional : ConditionalComponent
+    {
+        public HasStatusConditional(Loading.ConditionalData data)
+        {
+            mStatus = data.mData;
+        }
+        public override bool Check(Entities.Entity entity)
+        {
+            return ((Entities.Unit)entity).HasStatus(mStatus);
+        }
+        private string mStatus;
+    }
+
+    [Serializable()]
+    public class NoStatusConditional : ConditionalComponent
+    {
+        public NoStatusConditional(Loading.ConditionalData data)
+        {
+            mStatus = data.mData;
+        }
+        public override bool Check(Entities.Entity entity)
+        {
+            return !((Entities.Unit)entity).HasStatus(mStatus);
+        }
+        private string mStatus;
+    }
+
+    [Serializable()]
+    public class OrderNotUsedConditional : ConditionalComponent
+    {
+        public OrderNotUsedConditional(Loading.ConditionalData data)
+        {
+        }
+        public override bool Check(Entities.Entity entity)
+        {
+            Entities.Effect_Entity effectEntity = (Entities.Effect_Entity)entity;
+            Effects.Orders.Order order = (Effects.Orders.Order)(effectEntity.GetEffect());
+            return !order.IsOrderUsed();
+        }
+    }
+}
+
