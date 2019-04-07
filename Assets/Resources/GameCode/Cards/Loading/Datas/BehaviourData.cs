e7569c234d43be688dbd3faf100b9cd3e437d33d
Edd Sherlock <pulsarflux@googlemail.com>
2019-04-07 11:27:51 +0100

    Add a new loading data type, Behaviour, that others can derive from.
    Handles the data for the newly added 'components': conditionals and info tags; as a common feature for other loading data types.

---
+++ Assets/Resources/GameCode/Cards/Loading/Datas/BehaviourData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.GameCode.Cards.Components.Conditional;
+using Assets.GameCode.Cards.Components;
+
+namespace Assets.GameCode.Cards.Loading
+{
+    [Serializable()]
+    public class ConditionalData
+    {
+        public ConditionalData(string type, string data)
+        {
+            mType = type;
+            mData = data;
+        }
+        public string mType;
+        public string mData;
+    }
+
+    [Serializable()]
+    public class InfoTagData
+    {
+        public InfoTagData(string type, string value)
+        {
+            mType = type;
+            mTagValue = value;
+        }
+        public string mType;
+        public string mTagValue;
+    }
+
+    [Serializable()]
+    public class BehaviourData
+    {
+        public BehaviourData()
+        {
+            mConditionals = null;
+            mInfoTags = null;
+        }
+
+        public void AddConditional(ConditionalData cData)
+        {
+            if (mConditionals == null)
+            {
+                mConditionals = new List<ConditionalData>();
+            }
+            mConditionals.Add(cData);
+        }
+
+        public void AddInfoTag(InfoTagData itData)
+        {
+            if (mInfoTags == null)
+            {
+                mInfoTags = new List<InfoTagData>();
+            }
+            mInfoTags.Add(itData);
+        }
+
+
+        public List<ConditionalData> mConditionals;
+        public List<InfoTagData> mInfoTags;
+    }
+}
+
