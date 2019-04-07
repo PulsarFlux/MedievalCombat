31dcccb9f7337ff37d153db69031231e89bad2d5
Edd Sherlock <pulsarflux@googlemail.com>
2019-04-07 11:43:16 +0100

    Make ModuleData derive from BehaviourData and add a lifetime variable

--- Assets/Resources/GameCode/Cards/Loading/Datas/ModuleData.cs
+++ Assets/Resources/GameCode/Cards/Loading/Datas/ModuleData.cs
@@ -6,16 +6,18 @@ using System.Text;
 namespace Assets.GameCode.Cards.Loading
 {
     [Serializable()]
-    public class ModuleData
+    public class ModuleData : BehaviourData
     {
         public string Name;
         public ModuleType Type;
         public List<string> Data = new List<string>();
+        public int mLifetime;
         public ModuleData(string Name, string Type)
         {
             //TODO - Keep ModuleData constructor up to date
             this.Name = Name;
             this.Type = CardLoading.GetModuleTypeFromString(Type);
+            mLifetime = -1;
         }
         public void AddData(string D)
         {
