8e55777e1d415eddc9d3065687edb6f11080d35a
Edd Sherlock <pulsarflux@googlemail.com>
2019-04-07 11:57:30 +0100

    Add check lifetime function to module

--- Assets/Resources/GameCode/Cards/Modules/Module.cs
+++ Assets/Resources/GameCode/Cards/Modules/Module.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +9,22 @@ namespace Assets.GameCode.Cards.Modules
     public abstract class Module
     {
         public ModuleType Type { get; protected set; }
+        private int mMaxLifetime = -1;
+        private int mCurrentLifetime = 0;
         public abstract void Message();
         public abstract void Setup(Entities.Entity Parent, Loading.ModuleData MD);
         // Called on NewTurn on every module
         // Intended for management of statuses rather than actual effects.
         public abstract void NewTurnGeneric();
+        public void CheckLifetime(Entities.Unit parent)
+        {
+            if (mMaxLifetime > -1)
+            {
+                if (++mCurrentLifetime >= mMaxLifetime)
+                {
+                    parent.RemoveModule(this.Type, this);
+                }
+            }
+        }
     }
 }
