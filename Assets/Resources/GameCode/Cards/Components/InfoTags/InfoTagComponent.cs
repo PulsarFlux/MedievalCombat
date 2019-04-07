16b1f16445fd0bd5c5ea4b2b172140ed736e0854
Edd Sherlock <pulsarflux@googlemail.com>
2019-04-07 11:17:32 +0100

    Add info tag components
    These are XML elements that can be used to pass information to their parent in a more generic way than needing to use predefined attributes for each element

---
+++ Assets/Resources/GameCode/Cards/Components/InfoTags/InfoTagComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assets.GameCode.Cards.Components
+{
+    [Serializable()]
+    public class InfoTag
+    {
+        public InfoTag(Loading.InfoTagData data)
+        {
+            mType = data.mType;
+            mValue = data.mTagValue;
+        }
+
+        public string mType;
+        public string mValue;
+    }
+}
+
