012034c221acd45ea20281901db75cab3a543ebe
Edd Sherlock <pulsarflux@googlemail.com>
2019-08-28 20:22:30 +0100

    Add save game functionality to the pause menu
    Not implemented for non-campaign card games since they do not have a save slot associated with them yet.

--- Assets/Resources/GameCode/Menus/PauseMenu/PauseMenuUIManager.cs
+++ Assets/Resources/GameCode/Menus/PauseMenu/PauseMenuUIManager.cs
@@ -11,6 +11,56 @@ namespace Assets.GameCode.Menus
             StateHolder.StateManager.CloseMenu();
         }
 
+        public void SaveButtonPressed()
+        {
+            SaveGame();
+
+            // Might as well return to the game.
+            StateHolder.StateManager.CloseMenu();
+        }
+
+        public void SaveQuitButtonPressed()
+        {
+            SaveGame();
+
+            // This will close the menu as well since
+            // we load the next scene in exclusive mode.
+            StateHolder.StateManager.MoveToNextScene(GameScene.MainMenu);
+        }
+
+        private void SaveGame()
+        {
+            Saves.SaveGame saveGame = new Assets.GameCode.Saves.SaveGame();
+            GameScene currentScene = StateHolder.StateManager.GetCurrentScene();
+
+            saveGame.mLoadScene = currentScene;
+            saveGame.mMapState = StateHolder.StateManager.LoadMapState();
+            saveGame.mCampaignState = StateHolder.StateManager.LoadCampaignState();
+
+            // TODO: Note that saving from a non-campaign card game is not yet implemented
+            // since we have not gone through choosing a save slot in that case.
+            if ( // currentScene == GameScene.CardGame ||
+                currentScene == GameScene.CampaignCardGame)
+            {
+                saveGame.mCardGameState = StateHolder.StateManager.LoadCardGameState();
+            }
+            else
+            {
+                saveGame.mCardGameState = null;
+            }
+
+            // Saving non-campaign card games is not supported, see above
+            if (currentScene != GameScene.CardGame)
+            {
+                SaveDescription saveDesc = StateHolder.StateManager.GetCurrentSaveDesc();
+                Saves.SaveManager.SaveGame(saveDesc.slot, saveDesc.saveName, saveGame);
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning("Saving from a non-campaign card game is not supported");
+            }
+        }
+
         // Use this for initialization
         void Start () 
         {
